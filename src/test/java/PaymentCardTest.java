bc1e2935ffd810cb741a5472b8b0d8e280ced234
Peter Morrison <pmorrison234@gmail.com>
2023-01-23 15:51:48 +0000

    Me and Cordu are the best

---
+++ src/test/java/PaymentCardTest.java
@@ -0,0 +1,17 @@
+import junit.framework.TestCase;
+
+public class PaymentCardTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testGetCardNumber() {
+    }
+
+    public void testGetExpiryDate() {
+    }
+
+    public void testGetSecurityNumber() {
+    }
+}
